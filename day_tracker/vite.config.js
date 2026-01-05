26519796d67fdee8ba667bb37737a75fb4ee02f1
Ayberk Yilmaz <309940+ayberk@users.noreply.github.com>
2026-01-04 20:45:57 -0800

    Build: Use relative base URL for better portability

--- day_tracker/vite.config.js
+++ day_tracker/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/tools/day-tracker',
+  base: './',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
